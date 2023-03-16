0e54861444cdc24295f4e3947ffbe7456f1937cf
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2023-03-16 20:16:12 +0900

    詳細画面用のページを追加

---
+++ pages/_id/spotDetail.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-container>
+    </v-container>
+</template>
+
+<script>
+export default {
+    layout: "HomeLayout",
+    middleware: "auth",
+    components: {
+    }
+}
+</script>
