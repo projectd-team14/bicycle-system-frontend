720b4e8f20980b8d7f82fb8a959dda76d4d1077d
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2023-03-17 23:07:33 +0900

    return後のアクションを追加

--- components/Setting/CameraSetting.vue
+++ components/Setting/CameraSetting.vue
@@ -1,86 +1,106 @@
 <template>
-    <v-row>
-        <v-col cols="8">
-            <v-card
-                class="mx-auto"
-                max-height="600"
-                style="background: #2c2d3f;"
-            > 
-                <v-card-item>
-                    <v-card-title class="headline">動作設定</v-card-title>
-                    <v-col cols="12">
-                        <v-row>
-                            <v-col cols="6">
-                                <v-btn
-                                    block
-                                    style="background: linear-gradient(#e00083, #33005e);"
-                                    @click="onClickStartButton()"
-                                >
-                                    ON
-                                </v-btn>
-                            </v-col>
-                            <v-col cols="6">
-                                <v-btn
-                                    block
-                                    style="background: linear-gradient(#e00083, #33005e);"
-                                    @click="onClickStopButton()"
-                                >
-                                    OFF
-                                </v-btn>                          
-                            </v-col>                        
-                        </v-row>                        
-                    </v-col>
-                </v-card-item>
-            </v-card>
-        </v-col>
-        <v-col cols="4">
-            <v-card
-                class="mx-auto"
-                max-height="600"
-                style="background: #2c2d3f;"
-            > 
-                <v-card-item>
-                    <v-card-title class="headline">削除</v-card-title>
+    <v-container>
+        <v-row>
+            <v-col cols="8">
+                <v-card
+                    class="mx-auto"
+                    max-height="600"
+                    style="background: #2c2d3f;"
+                > 
+                    <v-card-item>
+                        <v-card-title class="headline">動作設定</v-card-title>
                         <v-col cols="12">
-                            <v-btn
-                                block
-                                outlined
-                                color="#e00083" 
-                                @click="onClickDeleteButton()"
-                            >
-                                Delete
-                            </v-btn> 
-                        </v-col>                        
-                </v-card-item>
-            </v-card>  
-        </v-col>
-    </v-row>
+                            <v-row>
+                                <v-col cols="6">
+                                    <v-btn
+                                        block
+                                        style="background: linear-gradient(#e00083, #33005e);"
+                                        @click="onClickStartButton()"
+                                    >
+                                        ON
+                                    </v-btn>
+                                </v-col>
+                                <v-col cols="6">
+                                    <v-btn
+                                        block
+                                        style="background: linear-gradient(#e00083, #33005e);"
+                                        @click="onClickStopButton()"
+                                    >
+                                        OFF
+                                    </v-btn>                          
+                                </v-col>                        
+                            </v-row>                        
+                        </v-col>
+                    </v-card-item>
+                </v-card>
+            </v-col>
+            <v-col cols="4">
+                <v-card
+                    class="mx-auto"
+                    max-height="600"
+                    style="background: #2c2d3f;"
+                > 
+                    <v-card-item>
+                        <v-card-title class="headline">削除</v-card-title>
+                            <v-col cols="12">
+                                <v-btn
+                                    block
+                                    outlined
+                                    color="#e00083" 
+                                    @click="onClickDeleteButton()"
+                                >
+                                    Delete
+                                </v-btn> 
+                            </v-col>                        
+                    </v-card-item>
+                </v-card>  
+            </v-col>
+        </v-row>    
+        <v-snackbar
+            v-model="snackBar"
+            multi-line
+            color="secondary"
+        >
+            {{ snackText }}
+        </v-snackbar>    
+    </v-container>
 </template>
   
 <script>
 export default {
     data() {
-        return{
+        return {
+            snackText: "",
+            snackBar: false
         }
     },
     methods: {
         onClickStartButton() {
             this.$axios.get("/api/start/" + this.$route.params.cameras_id)
             .then((response) => {
-                console.log(response.data);
+                this.snackText = response.data.message;
+                this.snackBar = true;
             })
         },
         onClickStopButton() {
             this.$axios.get("/api/stop/" + this.$route.params.cameras_id)
             .then((response) => {
-                console.log(response.data);
+                this.snackText = response.data.message;
+                this.snackBar = true;
             })
         },
         onClickDeleteButton() {
             this.$axios.get("/api/delete_camera/" + this.$route.params.cameras_id)
             .then((response) => {
-                console.log(response);
+                const spotsId = String(response.data.spots_id);
+                this.onLoadCameraData(spotsId);
+                this.$router.push("/" + spotsId + "/spotDetail");
             })
+        },
+        async onLoadCameraData(spotsId) {
+            this.$store.dispatch("cameraData/onLoadCameraData", {
+                spotsId: spotsId
+            });
         }
     }
 }
